0bcdf9e2190f99c1ba8260b275d78f9d8cd2afdd
Gennadii Zakharov <Gennadiy.Zakharov@gmail.com>
2025-11-06 16:47:56 +0000

    Improved ownership and borrowing module - WIP.

--- examples/00-own-and-borrow.rs
+++ examples/00-own-and-borrow.rs
@@ -1,12 +1,117 @@
+use rustcourse::*;
 
-// Importing functions from our lib.rs
-use own_and_borrow::{demo_mutability, demo_ownership, demo_primitives, demo_references};
+
+// Static string literal - a series of bytes on the stack
+pub const MSG_LITERAL: &str = "Hello, world!";
+
+pub fn demo_primitives() {
+    println!("\n>>> DEMO PRIMITIVES <<<");
+    println!("Primitive data types (int, float) work like in any other language.");
+    println!("However this is due to one trick that we explore in the next example.");
+    let a= 2; // Type is inferred
+    println!("All Rust variables are immutable by default.");
+    let mut b = 3; // Mo make a mutable variable we must declare it explicitly
+
+
+    let c = sum2(a, b); // To function we transfer copies of the values
+    println!("The sum of {} and {} is {}",a,b,c);
+
+    println!("To change the value of a variable, we need to make it mutable.");
+    // a = 5; // this won't work
+    b = 7; // This works fine
+
+    println!("However, we can shadow the previous variable and re-assign it. Even with changing type");
+    let c = sum2(a, b);
+    println!("The sum of {} and {} is {}",a,b,c);
+}
+
+pub fn demo_ownership() {
+    println!("\n>>> DEMO OWNERSHIP <<<");
+    println!("OWNERSHIP MODEL is a key concept in Rust ===");
+    println!("Each value has exactly one owner (variable)");
+    println!("When the owner goes out of scope, the value is dropped");
+
+    println!("\nSome simple types — like integers, floats, booleans, and characters — implement the Copy trait.");
+    println!("When you assign or pass them, the value is duplicated, not moved.");
+    println!("This is the reason why their behavior look sthe same as in other languages.");
+
+    println!("\nTo demonstrate ownership, we will use a function that takes ownership of a String variable.");
+
+
+    let msg = String::from(MSG_LITERAL); //
+
+    println!("Using function with a direct link, we transfer ownership");
+    print_with_own(msg);
+
+    println!("At the end of the function scope the variable was destroyed");
+    println!("We can't use the variable after the function call");
+    //print_with_own(msg);
+}
+
+pub fn demo_borrowing() {
+    println!("\n>>> DEMO BORROWING <<<");
+    println!("To allow a function to use variable, we pass to it reference");
+    println!("This process is called BORROWING");
+
+    let msg = String::from(MSG_LITERAL);
+    let msg_ref = &msg;
+    print_with_borrow(msg_ref);
+    println!("We still OWN the variable, so we can use the same reference many times");
+    println!("We can even make references from references");
+    print_with_borrow(&msg_ref);
+    println!("The variablbe will be destryoed when the owner goes out of scope");
+    println!("Rust ensures at compile time that the reference never outlives the variable owner");
+}
+
+pub fn demo_mutability() {
+    println!("\n>>> DEMO MUTABILITY <<<");
+    println!("To change a variable we must declare it as mutable");
+    println!("Now we can change variable content both directly, and via functions");
+    let mut mut_msg = String::from(MSG_LITERAL);
+    println!("The message is: {}", mut_msg);
+    // Now we can use functions that modify our structure
+    mut_msg.push_str(" is mutable!");
+    println!("Now the same variable holds the message: {}", mut_msg);
+
+    println!("\nVery often we create IMMUTABLE reference to a mutable structure. This ensures that variable won't be modified dby mistake");
+    let immute_ref = &mut_msg;
+    println!("We pass variable to println!() by immutable reference: {}", immute_ref);
+
+    println!("\nWe can also create MUTABLE reference to a mutable structure");
+    let mut_ref = &mut mut_msg;
+    println!("With this reference, we can modify the CONTENT of the variable");
+    mut_ref.push_str(", is mutable via mut ref!");
+    println!("Now the same variable holds the message: {}", mut_ref);
+    println!("We can have simultaneously either many immutable references, or ONE mutable");
+    println!("This makes Rust tread-safe, because we can't modify the same variable from different threads simultaneously");
+}
+//fn demo_function_mutability() {
+//     println!("\n>>> DEMO FUNCTION MUTABILITY <<<");
+//     println!("\nCreatign mutable variables inside functions has some tricks :)");
+//  // print_and_change_string(mut_ref);
+    // However, we can,t change the REFERENCE itself
+    // mut_struct_ref = String::from("Hello another world"); // this won't work
+
+
+    // fn print_and_change_string(msg: &mut String) {
+    //     println!("Message to change: {}", msg);
+    //     msg.push_str(" - was changes after printing");
+    // }
+    // We can create MUTABLE reference for an IMMUTABLE structure
+    // let mut mut_ref= &mut_msg;
+    // We can't modify the CONTENT of the structure
+    // mut_ref.push_str(", modified via mut ref"); // This won't work!!!
+    // But we can redirect the reference to another structure
+    // let another_string = String::from("Hello another world");
+    // mut_ref = &another_string;
+//}
 
 fn main() {
-
+    println!("This example demonstrates key Rust concepts: ownership, borrowing and mutability");
+    println!("Please explore the code and read comments - they are useful :)");
     demo_primitives();
     demo_ownership();
-    demo_references();
+    demo_borrowing();
     demo_mutability();
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,13 @@
-use String;
-// this is a string literal
-pub const MSG_LITERAL: &str = "Hello, world!";
+// library file that holds some shared functions, useful for demo
 
-pub fn demo_primitives() {
-    // Primitive data types are created on stack
-    // They works like in any other language
-    let a= 2;
-    let mut b = 3;
-    let c = sum2(a,b); // To function we transfer copies of the values
-    // We can use original a and b values after function
-    println!("The sum of {} and {} is {}",a,b,c);
-
-    // We can change value only for mutable variables
-    // a = 5; // this won't work
-    b = 7; // This works fine
-
-    // However, we can shadow previous variable and re-assign it.
-    // We can change variable type at this point and re-use name
-    let c = sum2(a,b);
-    println!("The sum of {} and {} is {}",a,b,c);
-}
-
-pub fn demo_ownership() {
-    // OWNERSHIP FOR DATA ON THE HEAP
-    // TRANSFERRING OWNERSHIP
-    // Dynamic data values are allocated on heap
-    // This variable is String type. But this is the link to
-    // dynamic String object
-    let msg = String::from(MSG_LITERAL);
-
-    // Using function with a direct link, we transfer ownership
-    print_string_own(msg);
-
-    // At the end of the function scope the variable was destroyed
-    // The second usage of the variable is not possible
-    // print_String_own(msg);
-}
-
-pub fn demo_references() {
-    // USING REFERENCES
-    // Making second string variable
-    let msg = String::from(MSG_LITERAL);
-    // Here we transfer to the function a _reference_ to the String structure
-    let msg_ref = &msg;
-    print_string(msg_ref);
-    // We still OWN the variable, so we can use the same reference many times
-    // We can even make references form references
-    print_string(&msg_ref);
-    // here we reach the end of the scope - the variable will be destroyed here
-}
-
-pub fn demo_mutability() {
-    // MUTABILITY AND REFERENCES
-    // To change the variable and assigned data structure, we need to specify it as mutable
-    let mut mut_msg = String::from(MSG_LITERAL);
-    // Now we can use functions that modify our structure
-    mut_msg.push_str(" is mutable!");
-
-    // We can create simple immutable references
-    let immute_ref = &mut_msg;
-    print_string(&immute_ref);
-
-    // Usually we create immutable reference to a MUTABLE structure
-    let mut_struct_ref = &mut mut_msg;
-    // With this reference, we can modify the CONTENT of the structure
-    mut_struct_ref.push_str(", is mutable via mut ref!");
-    print_and_change_string(mut_struct_ref);
-    // However, we can,t change the REFERENCE itself
-    // mut_struct_ref = String::from("Hello another world"); // this won't work
-
-    // We can create MUTABLE reference for an IMMUTABLE structure
-    let mut mut_ref= &mut_msg;
-    print_string(mut_ref);
-    // We can't modify the CONTENT of the structure
-    // mut_ref.push_str(", modified via mut ref"); // This won't work!!!
-    // But we can redirect the reference to another structure
-    let another_string = String::from("Hello another world");
-    mut_ref = &another_string;
-    print_string(mut_ref);
-}
-
-fn sum2(a:i32, b:i32) -> i32 {
+pub fn sum2(a:i32, b:i32) -> i32 {
     a+b // implicit return in Rust
 }
 
-fn print_string_own(msg: String) {
-    println!("The message form owner is: {}", msg)
+pub fn print_with_own(msg: String) {
+    println!("The message from owner is: {}", msg)
 }
 
-
-fn print_string(msg: &String) {
+pub fn print_with_borrow(msg: &String) {
     println!("Message: {}", msg)
-}
-
-fn print_and_change_string(msg: &mut String) {
-    println!("Message to change: {}", msg);
-    msg.push_str(" - was changes after printing");
 }
